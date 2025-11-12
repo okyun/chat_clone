eedf187950e2f46c6e1ba130d25c997d543bb011
okyun <dhrdbs8066@naver.com>
2025-11-12 18:15:52 +0900

    build 파일 커핏

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ subprojects {
         testImplementation("org.springframework.boot:spring-boot-starter-test")
         testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
         testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+        implementation("org.springframework.boot:spring-boot-starter-data-redis")
     }
 
     tasks.withType<Test> {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,11 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 rootProject.name = "chat-pratice"
+
+include(
+    "chat-application",
+    "chat-domain",
+    "chat-persistence"
+    ,"chat-websocket"
+    ,"chat-api"
+)
